631e3a9c25be0b08d36709d18a21335848ada9fc
Matt <mateohdez950@gmail.com>
2025-09-12 12:02:59 -0600

    cambio en puertos

--- seguidor_v1.py
+++ seguidor_v1.py
@@ -15,16 +15,16 @@ def enciende_ev3():
     boton = TouchSensor()
     assert boton.connected
     #conectamos los motores
-    motor_izq = LargeMotor(OUTPUT_A)
+    motor_izq = LargeMotor(OUTPUT_D)
     assert motor_izq.connected
-    motor_der = LargeMotor(OUTPUT_B)
+    motor_der = LargeMotor(OUTPUT_A)
     assert motor_der.connected
     #conectamos los sensores de color
-    ojo_izq = ColorSensor('in1')
+    ojo_izq = ColorSensor('in3')
     assert ojo_izq.connected
     ojo_med = ColorSensor('in2')
     assert ojo_med.connected
-    ojo_der = ColorSensor('in3')
+    ojo_der = ColorSensor('in1')
     assert ojo_der.connected
     #cambiamos el modo del sensor de color
     ojo_izq.mode = 'COL-REFLECT'
